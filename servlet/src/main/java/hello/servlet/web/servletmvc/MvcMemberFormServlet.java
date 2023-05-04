36ca4a8ac57a88a13cc7163577676d3e8e7a7f4f
ykmxxi <ykmxxi97@gmail.com>
2023-05-04 11:18:40 +0900

    Implement #3- mvc pattern by servlet and jsp
    - jsp와 servlet을 이용한 mvc 패턴 적용
    - servlet: 컨트롤러 역할
    - jsp: 뷰 역할
    - `HttpServletRequest` 객체: 모델 역할

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberFormServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.web.servletmvc;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 컨트롤러
+ */
+@WebServlet(name = "mvcMemberFormServlet", urlPatterns = "/servlet-mvc/members/new-form")
+public class MvcMemberFormServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String viewPath = "/WEB-INF/views/new-form.jsp";
+		// 컨트롤러에서 뷰로 이동
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		dispatcher.forward(request, response);
+	}
+}
