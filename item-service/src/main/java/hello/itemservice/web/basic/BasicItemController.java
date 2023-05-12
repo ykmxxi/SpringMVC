ecbab3039531dd80d2553770e2dbd0bda099b8dc
ykmxxi <ykmxxi97@gmail.com>
2023-05-12 11:55:29 +0900

    Implement #11- item save controller
    - 상품 저장 컨트롤러 구현
    - 상품 저장 후 뷰는 기존 상품 상세 뷰 이용

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,8 +7,11 @@ import javax.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
@@ -40,6 +43,60 @@ public class BasicItemController {
 	@GetMapping("/add")
 	public String addForm() {
 		return "basic/addForm";
+	}
+
+	//	@PostMapping("/add")
+	public String addItemV1(
+		@RequestParam String itemName,
+		@RequestParam Integer price,
+		@RequestParam Integer quantity,
+		Model model
+	) {
+		Item item = new Item();
+		item.setItemName(itemName);
+		item.setPrice(price);
+		item.setQuantity(quantity);
+
+		itemRepository.save(item);
+		model.addAttribute("item", item);
+
+		return "/basic/item";
+	}
+
+	/**
+	 * @ModelAttribute("item") Item item
+	 * model.addAttribute("item", item); 자동 추가
+	 */
+//	@PostMapping("/add")
+	public String addItemV2(@ModelAttribute("item") Item item) {
+		itemRepository.save(item);
+//		model.addAttribute("item", item); // 생략 가능, 자동 추가됨
+
+		return "/basic/item";
+	}
+
+	/**
+	 * @ModelAttribute name 생략 가능
+	 * model.addAttribute(item); 자동 추가, 생략 가능
+	 * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+	 */
+//	@PostMapping("/add")
+	public String addItemV3(@ModelAttribute Item item) {
+		itemRepository.save(item);
+
+		return "/basic/item";
+	}
+
+	/**
+	 * @ModelAttribute name 생략 가능
+	 * model.addAttribute(item); 자동 추가, 생략 가능
+	 * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
+	 */
+	@PostMapping("/add")
+	public String addItemV4(Item item) {
+		itemRepository.save(item);
+
+		return "/basic/item";
 	}
 
 	/**
