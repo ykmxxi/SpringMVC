8107545888c11b7a451c662f7804646a8c82d38a
ykmxxi <ykmxxi97@gmail.com>
2023-05-13 12:12:48 +0900

    Implement #11- apply PRG in item add controller
    - 상품 저장 컨트롤러에 PRG 적용

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -92,11 +92,21 @@ public class BasicItemController {
 	 * model.addAttribute(item); 자동 추가, 생략 가능
 	 * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록 Item -> item
 	 */
-	@PostMapping("/add")
+//	@PostMapping("/add")
 	public String addItemV4(Item item) {
 		itemRepository.save(item);
 
 		return "/basic/item";
+	}
+
+	/**
+	 * PRG - Post/Redirect/Get
+	 */
+	@PostMapping("/add")
+	public String addItemV5(Item item) {
+		itemRepository.save(item);
+
+		return "redirect:/basic/items/" + item.getId();
 	}
 
 	@GetMapping("/{itemId}/edit")
