e9988773c937ba0152ab1c691ea11c1454c0753f
ykmxxi <ykmxxi97@gmail.com>
2023-05-15 12:55:46 +0900

    Implement #13- using SpringEL variable
    - 변수 표현식 `${...}` 사용
    - 타임리프 지역 변수 `th:with`사용

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,9 +1,16 @@
 package hello.thymeleaf.basic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
 
 @Controller
 @RequestMapping("/basic")
@@ -22,6 +29,43 @@ public class BasicController {
 	public String textUnescaped(Model model) {
 		model.addAttribute("data", "Hello <b>Spring!</b>");
 		return "basic/text-unescaped";
+	}
+
+	/**
+	 * SpringEL 변수 표현식
+	 */
+	@GetMapping("/variable")
+	public String variable(Model model) {
+		User userA = new User("userA", 20);
+		User userB = new User("userB", 22);
+
+		List<User> list = new ArrayList<>();
+		list.add(userA);
+		list.add(userB);
+
+		Map<String, User> map = new HashMap<>();
+		map.put("userA", userA);
+		map.put("userB", userB);
+
+		model.addAttribute("user", userA);
+		model.addAttribute("users", list);
+		model.addAttribute("userMap", map);
+
+		return "/basic/variable";
+	}
+
+	/**
+	 * Sample data
+	 */
+	@Data
+	static class User {
+		private String username;
+		private int age;
+
+		public User(String username, int age) {
+			this.username = username;
+			this.age = age;
+		}
 	}
 
 }
