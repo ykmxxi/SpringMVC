c2a68edb4003399cb88a05cbda7be3dadefdcbab
ykmxxi <ykmxxi97@gmail.com>
2023-05-17 15:05:58 +0900

    Add #15- thymeleaf radio button
    - 상품 등록 폼 라디오 버튼 추가
    - 상품 상세 폼 라디오 버튼 추가
    - 상품 수정 폼 라디오 버튼 추가
    - `@ModelAttribute`로 상품 타입 정보를 모델에 담음

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -15,6 +15,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -38,6 +39,11 @@ public class FormItemController {
 		regions.put("JEJU", "제주");
 
 		return regions;
+	}
+
+	@ModelAttribute("itemTypes")
+	public ItemType[] itemTypes() {
+		return ItemType.values();
 	}
 
 	@GetMapping
@@ -64,6 +70,7 @@ public class FormItemController {
 	public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 		log.info("item.open={}", item.getOpen());
 		log.info("item.regions={}", item.getRegions());
+		log.info("item.itemType={}", item.getItemType());
 
 		Item savedItem = itemRepository.save(item);
 		redirectAttributes.addAttribute("itemId", savedItem.getId());
