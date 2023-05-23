d3c0c8ddd3297eae2f330304cff5887bc19e432f
ykmxxi <ykmxxi97@gmail.com>
2023-05-23 14:44:11 +0900

    Refactor #21- validator seperation
    - 검증 로직을 컨트롤러에서 분리
    - 검증 로직 직접 호출

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -27,6 +27,7 @@ import lombok.extern.slf4j.Slf4j;
 public class ValidationItemControllerV2 {
 
 	private final ItemRepository itemRepository;
+	private final ItemValidator itemValidator;
 
 	@GetMapping
 	public String items(Model model) {
@@ -181,7 +182,7 @@ public class ValidationItemControllerV2 {
 		return "redirect:/validation/v2/items/{itemId}";
 	}
 
-	@PostMapping("/add")
+	// @PostMapping("/add")
 	public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
 		RedirectAttributes redirectAttributes, Model model) {
 
@@ -214,6 +215,25 @@ public class ValidationItemControllerV2 {
 		if (bindingResult.hasErrors()) {
 			log.info("errors={}", bindingResult);
 
+			return "validation/v2/addForm";
+		}
+
+		// 검증 성공 로직
+		Item savedItem = itemRepository.save(item);
+		redirectAttributes.addAttribute("itemId", savedItem.getId());
+		redirectAttributes.addAttribute("status", true);
+
+		return "redirect:/validation/v2/items/{itemId}";
+	}
+
+	@PostMapping("/add")
+	public String addItemV5(@ModelAttribute Item item, BindingResult bindingResult,
+		RedirectAttributes redirectAttributes, Model model) {
+
+		itemValidator.validate(item, bindingResult);
+
+		if (bindingResult.hasErrors()) {
+			log.info("errors={}", bindingResult);
 			return "validation/v2/addForm";
 		}
 
