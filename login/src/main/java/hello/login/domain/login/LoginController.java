bbbd7e8d50a414a832af79d8b66f7e1b833a8ed5
ykmxxi <ykmxxi97@gmail.com>
2023-05-27 14:54:00 +0900

    Implement #27- login feature
    - 로그인 기능 구현

---
+++ login/src/main/java/hello/login/domain/login/LoginController.java
@@ -0,0 +1,46 @@
+package hello.login.domain.login;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import hello.login.domain.member.Member;
+import hello.login.web.login.LoginForm;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+	private final LoginService loginService;
+
+	@GetMapping("/login")
+	public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+		return "login/loginForm";
+	}
+
+	@PostMapping("/login")
+	public String login(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "login/loginForm";
+		}
+
+		Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+		if (loginMember == null) {
+			bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+			return "login/loginForm";
+		}
+
+		// TODO: 로그인 성공 처리
+
+		return "redirect:/";
+	}
+
+}
