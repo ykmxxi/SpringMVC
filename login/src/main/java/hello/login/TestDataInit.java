a30c763aff25302151414b55df689c1b6002bc53
ykmxxi <ykmxxi97@gmail.com>
2023-05-27 14:13:35 +0900

    Add #27- member data for test

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 public class TestDataInit {
 
 	private final ItemRepository itemRepository;
+	private final MemberRepository memberRepository;
 
 	/**
 	 * 테스트용 데이터 추가
@@ -21,6 +24,13 @@ public class TestDataInit {
 	public void init() {
 		itemRepository.save(new Item("itemA", 10000, 10));
 		itemRepository.save(new Item("itemB", 20000, 20));
+
+		Member member = new Member();
+		member.setLoginId("test");
+		member.setPassword("test!");
+		member.setName("테스터");
+
+		memberRepository.save(member);
 	}
 
 }
