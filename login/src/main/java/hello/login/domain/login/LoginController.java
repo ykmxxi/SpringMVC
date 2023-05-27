ed8b89baadf4818fc46d61a0b89438c901f52fd1
ykmxxi <ykmxxi97@gmail.com>
2023-05-27 15:42:59 +0900

    Implement #27- logout feature by session cookie
    - 세션 쿠키를 이용해 로그아웃 기능 구현

--- login/src/main/java/hello/login/domain/login/LoginController.java
+++ login/src/main/java/hello/login/domain/login/LoginController.java
@@ -47,4 +47,17 @@ public class LoginController {
 		return "redirect:/";
 	}
 
+	@PostMapping("/logout")
+	public String logout(HttpServletResponse response) {
+		expireCookie(response, "memberId");
+
+		return "redirect:/";
+	}
+
+	private static void expireCookie(HttpServletResponse response, String cookieName) {
+		Cookie cookie = new Cookie(cookieName, null);
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+	}
+
 }
