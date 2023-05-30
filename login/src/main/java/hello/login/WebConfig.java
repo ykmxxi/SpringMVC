d611c068354ce8347629903d6b18bdb939e5f4f3
ykmxxi <ykmxxi97@gmail.com>
2023-05-30 17:59:13 +0900

    Implement #29- log filter by servlet filter
    - 서블릿 필터로 요청 로그를 남기는 필터 구현

---
+++ login/src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,23 @@
+package hello.login;
+
+import javax.servlet.Filter;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.login.web.filter.LogFilter;
+
+@Configuration
+public class WebConfig {
+
+	@Bean
+	public FilterRegistrationBean logFilter() {
+		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+		filterRegistrationBean.setFilter(new LogFilter());
+		filterRegistrationBean.setOrder(1);
+		filterRegistrationBean.addUrlPatterns("/*");
+
+		return filterRegistrationBean;
+	}
+}
