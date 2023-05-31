359ce6b65e4a37679158d63bf19e605e0542c822
ykmxxi <ykmxxi97@gmail.com>
2023-05-31 15:04:34 +0900

    Implement #29- login interceptor by spring interceptor
    - 스프링 인터셉터로 로그인 체크 인터셉트 구현

--- login/src/main/java/hello/login/WebConfig.java
+++ login/src/main/java/hello/login/WebConfig.java
@@ -3,7 +3,6 @@ package hello.login;
 import javax.servlet.Filter;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
 import hello.login.web.interceptor.LogInterceptor;
+import hello.login.web.interceptor.LoginCheckInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -21,6 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
 			.order(1)
 			.addPathPatterns("/**")
 			.excludePathPatterns("/css/**", "/*.ico", "/error");
+
+		registry.addInterceptor(new LoginCheckInterceptor())
+			.order(2)
+			.addPathPatterns("/**")
+			.excludePathPatterns("/", "/members/add", "/login", "/logout", "/css/**", "/*.ico", "/error");
 	}
 
 	// @Bean
@@ -33,7 +38,7 @@ public class WebConfig implements WebMvcConfigurer {
 		return filterRegistrationBean;
 	}
 
-	@Bean
+	// @Bean
 	public FilterRegistrationBean loginCheckFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
 		filterRegistrationBean.setFilter(new LoginCheckFilter());
