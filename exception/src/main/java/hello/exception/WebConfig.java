32d5ca37262ac78d6d5f1a4a5cd7d1f60a7ad038
ykmxxi <ykmxxi97@gmail.com>
2023-06-01 17:13:15 +0900

    Implement #31- spring interceptor and dispatcher type

--- exception/src/main/java/hello/exception/WebConfig.java
+++ exception/src/main/java/hello/exception/WebConfig.java
@@ -4,16 +4,28 @@ import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
 
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-	@Bean
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new LogInterceptor())
+			.order(1)
+			.addPathPatterns("/**")
+			.excludePathPatterns(
+				"/css/**", "/*.ico"
+				, "/error", "/error-page/**" // 오류 페이지 경로
+			);
+	}
+
+	// @Bean
 	public FilterRegistrationBean logFilter() {
 		FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
 		filterRegistrationBean.setFilter(new LogFilter());
