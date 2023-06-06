c6b468076e0afb6659a2298ba65db6264368a0a4
ykmxxi <ykmxxi97@gmail.com>
2023-06-06 12:54:00 +0900

    Implement #33- DefaultHandlerExceptionResolver by Spring Boot
    - 스프링 부트가 제공하는 스프링 예외 처리
    - TypeMismatchException

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -3,6 +3,7 @@ package hello.exception.api;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -40,6 +41,11 @@ public class ApiExceptionController {
 	@GetMapping("/api/response-status-ex2")
 	public String responseStatusEx2() {
 		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
+	}
+
+	@GetMapping("/api/default-handler-ex")
+	public String defaultException(@RequestParam Integer data) {
+		return "ok";
 	}
 
 	@Data
