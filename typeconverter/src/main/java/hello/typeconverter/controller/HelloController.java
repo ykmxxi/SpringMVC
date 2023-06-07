2ea49f1ec3386039c3e593eb3b236565bc861233
ykmxxi <ykmxxi97@gmail.com>
2023-06-07 12:03:39 +0900

    Implement #35- basic type converter controller

---
+++ typeconverter/src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello-v1")
+	public String helloV1(HttpServletRequest request) {
+
+		String data = request.getParameter("data"); // 요청 파라미터는 문자 타입으로 조회
+		Integer intValue = Integer.valueOf(data); // 요청 파라미터를 숫자 타입으로 변환
+
+		System.out.println("intValue = " + intValue);
+		return "ok";
+	}
+
+	@GetMapping("/hello-v2")
+	public String helloV1(@RequestParam Integer data) {
+
+		System.out.println("data = " + data);
+		return "ok";
+	}
+
+}
